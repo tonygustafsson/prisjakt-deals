03067611677c4ab32a1447bfc3e9150f9197a0b1
Tony Gustafsson <mail@tonyg.se>
2022-11-17 22:57:58 +0100

    Compare with prev result, only show new products

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const puppeteer = require("puppeteer");
+const fs = require("fs");
 
+// Settings
 const baseUrl =
   "https://www.prisjakt.nu/tema/dagens-deals?sort=price.diff_percentage%7Cdesc";
 const pageSize = 24;
-const pagesToScrape = 4;
+const pagesToScrape = 3;
+const dataFolder = "./data";
+const dataFile = `${dataFolder}/database.json`;
 
 const scrape = async (url) => {
   console.log(`Scraping ${url}...`);
@@ -29,8 +33,10 @@ const scrape = async (url) => {
         const category = card.querySelector(
           "[class^='CardContent'] > span:nth-child(2)"
         )?.innerText;
+        const id = url.split("=")[1];
 
         return {
+          id,
           title,
           category,
           percentageOff,
@@ -53,5 +59,39 @@ const scrape = async (url) => {
     )
   );
 
-  console.log(productCards.flat());
+  // Convert the page arrays to a single object where the id is key and body is the value
+  const data = productCards
+    .flat()
+    .reduce((acc, product) => ({ ...acc, [product.id]: product }), {});
+  const json = JSON.stringify(data);
+  let previousData = {};
+
+  if (fs.existsSync(dataFile)) {
+    const database = fs.readFileSync(dataFile);
+    previousData = JSON.parse(database);
+  }
+
+  const newProducts = Object.keys(data).reduce((acc, id) => {
+    if (!previousData[id] || previousData[id].price !== data[id].price) {
+      return [...acc, data[id]];
+    }
+
+    return acc;
+  }, []);
+
+  if (!newProducts.length) {
+    console.log("No new products");
+  } else {
+    console.log(newProducts);
+  }
+
+  // Be sure that the data folder exists
+  if (!fs.existsSync(dataFolder)) {
+    fs.mkdirSync(dataFolder);
+  }
+
+  // Save data to file
+  fs.writeFileSync(dataFile, json, function (err) {
+    if (err) return console.log(err);
+  });
 })();
