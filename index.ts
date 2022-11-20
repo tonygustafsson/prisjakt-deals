fb4a9c48b178f0f7d3015b26d036fcf5ae7cb1fc
Tony Gustafsson <mail@tonyg.se>
2022-11-20 07:37:13 +0100

    Backtrack

--- index.ts
+++ index.ts
@@ -15,7 +15,34 @@ const scrape = async (url: string) => {
   // Map through all the products on the current page
   const productCards = await page.$$eval(
     "li[data-test='ProductGridCard']",
-    (cards: Element[]) => cards.map((card) => normalizeProduct(card))
+    (cards: any[]) =>
+      cards.map((product) => {
+        const title = product.querySelector(
+          "[data-test='ProductName']"
+        )?.textContent;
+        const percentageOff = product.querySelector(
+          "[class^='DiffPercentage']"
+        )?.textContent;
+        const url = product
+          .querySelector("[class^='InternalLink']")
+          ?.getAttribute("href");
+        const price = product.querySelector(
+          "[class*='BaseButton']"
+        )?.textContent;
+        const category = product.querySelector(
+          "[class^='CardContent'] > span:nth-child(2)"
+        )?.textContent;
+        const id = url?.split("=")[1] || "0";
+
+        return {
+          id,
+          title,
+          category,
+          percentageOff,
+          price,
+          url,
+        } as Product;
+      })
   );
 
   browser.close();
